8668a2759d50211bfcb8e4604b3b6b132ea570f4
Aaron Edsinger <aedsinger@hello-robot.com>
2020-05-26 09:05:21 -0700

    Added arduino install

--- factory/stretch_factory_dev_setup.sh
+++ factory/stretch_factory_dev_setup.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-
-pip install --user --upgrade twine
-
-# install  typora
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-sudo add-apt-repository 'deb https://typora.io/linux ./'
-sudo apt-get update
-sudo apt-get install typora
-
-#Install arduino
-
-#Install PyCharm
-
 #####################################################
 echo "To be run after a user install "
 echo "Configures to use local stretch_body"
@@ -25,26 +12,46 @@ then
     exit 1
 fi
 
-pip uninstall stretch-body
+
+pip install --user --upgrade twine
+
+# install  typora
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt install --yes typora
+
+sudo apt install --yes mkdocs
+sudo apt install --yes chromium-browser
+#Install arduino
+./arduino_install.sh -c 64 1.8.12 ~/
+cd ~/arduino
+sudo ./install.sh
+
+#Install PyCharm
+sudo snap install pycharm-community --classic
+
+#pip uninstall stretch-body
 echo "Cloning repos."
 cd ~/repos/
-git clone https://github.com/hello-robot/stretch_factory.git
 git clone https://github.com/hello-robot/stretch_install.git
 git clone https://github.com/hello-robot/stretch_body.git
 git clone https://github.com/hello-robot/stretch_firmware.git
 git clone https://github.com/hello-robot/stretch_fleet.git
 git clone https://github.com/hello-robot/stretch_sandbox.git
+git clone https://github.com/hello-robot/hello-robot.github.io
+git clone https://github.com/hello-robot/stretch_docs
 echo "Done."
 echo ""
 
 
 # update .bashrc to add body code directory to the Python path
 
-echo "export PATH=\${PATH}:~/repos/stretch_body/python/bin" >> ~/.bashrc
-echo "export PATH=\${PATH}:~/repos/stretch_factory/python/tools" >> ~/.bashrc
+#echo "export PATH=\${PATH}:~/repos/stretch_body/python/bin" >> ~/.bashrc
+#echo "export PATH=\${PATH}:~/repos/stretch_factory/python/tools" >> ~/.bashrc
 
-echo "export PYTHONPATH=\${PYTHONPATH}:~/repos/stretch_factory/python/tools" >> ~/.bashrc
-echo "export PYTHONPATH=\${PYTHONPATH}:~/repos/stretch_body/python" >> ~/.bashrc
+#echo "export PYTHONPATH=\${PYTHONPATH}:~/repos/stretch_factory/python/tools" >> ~/.bashrc
+#echo "export PYTHONPATH=\${PYTHONPATH}:~/repos/stretch_body/python" >> ~/.bashrc
 
-echo "Source new bashrc now...Done."
-echo ""
+#echo "Source new bashrc now...Done."
+#echo ""
