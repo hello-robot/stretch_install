51bf7f01d7c2d7321067fdb59829ad9d0195a130
hello-binit <64861565+hello-binit@users.noreply.github.com>
2020-08-04 01:06:54 -0400

    Full reboot notice

--- stretch_update.sh
+++ stretch_update.sh
@@ -3,3 +3,10 @@ git pull
 ./stretch_install_system.sh |& tee ~/stretch_user/log/stretch_system_update.log
 ./stretch_install_user.sh |& tee ~/stretch_user/log/stretch_user_update.log
 
+echo "#############################################"
+echo "A full reboot is recommended. Shutdown Ubuntu"
+echo "as usual, then use the power switch in the"
+echo "robot's trunk to cut power. Note that the"
+echo "lift will drop when power is cut. Use the"
+echo "included clamp to secure the lift."
+echo "#############################################"
