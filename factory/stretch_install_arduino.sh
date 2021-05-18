63146841f5b37f880487a5625a92659f2a52c2d4
Aaron Edsinger <aedsinger@hello-robot.com>
2021-05-18 13:23:49 -0700

    wip

--- factory/stretch_install_arduino.sh
+++ factory/stretch_install_arduino.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#To install IDE
+# ./arduino_install_ide.sh -c 64 1.8.12 ~/
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$HOME/.local/bin/ sh
 arduino-cli config init
 arduino-cli core install arduino:samd@1.6.21
