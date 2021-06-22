862e388c340d115fcc18b513ceb0ece8d696bb49
Binit Shah <64861565+hello-binit@users.noreply.github.com>
2021-06-22 15:16:12 -0400

    Update ROS Apt Repo GPG key

--- stretch_install_system.sh
+++ stretch_install_system.sh
@@ -63,8 +63,8 @@ echo "MAIN INSTALLATION OF ROS NOETIC"
 echo "Setting up sources.list"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo "Setting up keys"
-# New key as of Jun 8, 2019
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+# New key as of Jun 22, 2021
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 echo "apt update"
 sudo apt --yes update
 echo "Installating Desktop-Full Version"
