15ac456ed3dbfceae6e4a19796334b65a4ee46f2
Binit Shah <bshah@hello-robot.com>
2021-11-22 21:39:32 -0800

    autostart xbox_teleop with py3

--- factory/hello_robot_xbox_teleop.sh
+++ factory/hello_robot_xbox_teleop.sh
@@ -3,5 +3,5 @@
 . /etc/hello-robot/hello-robot.conf
 export HELLO_FLEET_ID HELLO_FLEET_ID
 export HELLO_FLEET_PATH=$HOME/stretch_user
-/usr/bin/python $HOME/.local/bin/stretch_xbox_controller_teleop.py
+/usr/bin/python3 $HOME/.local/bin/stretch_xbox_controller_teleop.py
 
