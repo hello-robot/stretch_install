2dd2beba82e8b465c13cdeeb39647d43a8d3fa9a
Mohamed Fazil <97637212+hello-fazil@users.noreply.github.com>
2022-02-28 13:12:56 -0800

    Stretch Description xarco path typo correction.

--- factory/stretch_install_dex_wrist.sh
+++ factory/stretch_install_dex_wrist.sh
@@ -27,7 +27,7 @@ cd ~/repos/stretch_tool_share
 git pull
 cd ~/repos/stretch_tool_share/tool_share/stretch_dex_wrist/stretch_description
 cp urdf/stretch_dex_wrist.xacro ~/catkin_ws/src/stretch_ros/stretch_description/urdf
-cp urdf/stretch_descripton.xacro ~/catkin_ws/src/stretch_ros/stretch_description/urdf
+cp urdf/stretch_description.xacro ~/catkin_ws/src/stretch_ros/stretch_description/urdf
 cp meshes/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes
 
 
