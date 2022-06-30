1ceb0c146e1588eae0d035cdcd461023a23615b5
Aaron Edsinger <aedsinger@hello-robot.com>
2022-06-30 14:20:16 -0700

    fix dexwrist install

--- factory/stretch_install_dex_wrist.sh
+++ factory/stretch_install_dex_wrist.sh
@@ -3,6 +3,7 @@
 #####################################################
 echo "To be run once a robot has gone through production bringup "
 echo "Configures to use an attached DexWrist"
+echo "Use --factory if this is a factory installation"
 
 read -p "Proceed with installation (y/n)?" -n 1 -r
 echo
@@ -15,7 +16,7 @@ echo "Setting Dynamixel bauds to 115200"
 RE1_dynamixel_set_baud.py /dev/hello-dynamixel-wrist 13 115200
 
 echo "Configuring user YAML"
-./stretch_dex_wrist_yaml_configure.py
+./stretch_dex_wrist_yaml_configure.py &1
 
 echo "Setting up Stretch ROS and URDF"
 cd ~/catkin_ws/src/stretch_ros/
