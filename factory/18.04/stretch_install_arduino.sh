e9081ccf3cc746868b76a55b6c348f5b1ffbcdad
Binit Shah <bshah@hello-robot.com>
2022-08-10 19:20:34 -0700

    Fix arduino install

--- factory/18.04/stretch_install_arduino.sh
+++ factory/18.04/stretch_install_arduino.sh
@@ -5,29 +5,8 @@
 #cd ~/arduino
 #sudo ./install.sh
 
-ARDUINOCLI_YAML="
-board_manager:
-  additional_urls: []
-daemon:
-  port: '50051'
-directories:
-  data: $HOME/.arduino15
-  downloads: $HOME/.arduino15/staging
-  user: $HOME/repos/stretch_firmware/arduino
-library:
-  enable_unsafe_install: false
-logging:
-  file: ''
-  format: text
-  level: info
-sketch:
-  always_export_binaries: false
-telemetry:
-  addr: :9090
-  enabled: true
-"
-
+#To install CLI
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$HOME/.local/bin/ sh
 $HOME/.local/bin/arduino-cli config init
 $HOME/.local/bin/arduino-cli core install arduino:samd@1.6.21
-echo $ARDUINOCLI_YAML >> ~/.arduino15/arduino-cli.yaml
+sed -i -e 's#Arduino#repos/stretch_firmware/arduino#g' ~/.arduino15/arduino-cli.yaml
