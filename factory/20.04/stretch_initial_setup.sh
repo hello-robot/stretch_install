7e5c192ab3b73c6df95be4976cf94aec7918ebf5
Aaron Edsinger <aedsinger@hello-robot.com>
2022-08-22 12:06:49 -0700

    ready to merge audio work

--- factory/20.04/stretch_initial_setup.sh
+++ factory/20.04/stretch_initial_setup.sh
@@ -99,7 +99,6 @@ sudo cp $DIR/hello_sudoers /etc/sudoers.d/
 echo "Setting up startup scripts..."
 mkdir -p ~/.local/bin
 sudo cp $DIR/hello_robot_audio.sh /usr/bin/
-sudo cp $DIR/sleep.py /usr/bin/
 sudo cp $DIR/hello_robot_lrf_off.py /usr/bin/
 sudo cp $DIR/hello_robot_pimu_ping.py /usr/bin/
 sudo cp $DIR/hello_robot_pimu_ping.sh /usr/bin/
