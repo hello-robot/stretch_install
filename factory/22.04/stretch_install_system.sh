6c5ccfba62c6f6fab8bea49e1b665b58c5016290
hello-chintan <cdesai@hello-robot.com>
2023-05-11 15:14:36 -0700

    Update system install script

--- factory/22.04/stretch_install_system.sh
+++ factory/22.04/stretch_install_system.sh
@@ -60,110 +60,85 @@ echo "Install BleachBit"
 install bleachbit
 echo ""
 
-# see http://wiki.ros.org/noetic/Installation/Ubuntu for details
-echo "###########################################"
-echo "INSTALLATION OF ROS NOETIC"
-echo "###########################################"
-echo "Setting up sources.list"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-echo "Setting up keys"
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - &>> $REDIRECT_LOGFILE
-echo "Apt update"
-sudo apt-get --yes update >> $REDIRECT_LOGFILE
-echo "Install ROS Noetic (this might take a while)"
-install ros-noetic-desktop-full
-echo "Install rosdep"
-install python3-rosdep
-echo "Configure rosdep"
-if [ -f "/etc/ros/rosdep/sources.list.d/20-default.list" ]; then
-    sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
-fi
-sudo rosdep init >> $REDIRECT_LOGFILE
-rosdep update --include-eol-distros >> $REDIRECT_LOGFILE
-echo "Install vcstool"
-install python3-vcstool
-echo ""
-
 # see https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html for details
 echo "###########################################"
-echo "INSTALLATION OF ROS2 GALACTIC"
+echo "INSTALLATION OF ROS 2 HUMBLE"
 echo "###########################################"
+
+echo "Ensuring Ubuntu Universe repository is enabled"
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
 echo "Setting up keys"
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
 echo "Setting up sources.list"
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
 echo "Apt update"
 sudo apt-get --yes update >> $REDIRECT_LOGFILE
-echo "Install ROS2 Galactic (this might take a while)"
-install ros-galactic-desktop
+
+echo "Install ROS 2 Humble (this might take a while)"
+install ros-humble-desktop
+
 echo "Install colcon"
 install python3-colcon-common-extensions
 echo ""
 
 echo "###########################################"
-echo "INSTALLATION OF ADDITIONAL ROS NOETIC/GALACTIC PKGS"
+echo "INSTALLATION OF ADDITIONAL ROS HUMBLE PKGS"
 echo "###########################################"
 echo "Install packages to work with URDFs"
 install liburdfdom-tools meshlab
-install ros-noetic-urdfdom-py
-install ros-galactic-urdfdom-py
+install ros-humble-urdfdom-py
+
 echo "Install joint state GUI package"
-install ros-noetic-joint-state-publisher-gui
-install ros-galactic-joint-state-publisher-gui
+install ros-humble-joint-state-publisher-gui
+
 echo "Install TF2 related packages"
-install ros-noetic-tf2-tools
-install ros-galactic-tf2-tools
+install ros-humble-tf2-tools
+
 echo "Install IMU visualization plugin for RViz and IMU filter"
-install ros-noetic-rviz-imu-plugin ros-noetic-imu-filter-madgwick
-install ros-galactic-rviz-imu-plugin ros-galactic-imu-filter-madgwick
-echo "Install robot pose filter for use with IMU and wheel odometry"
-install ros-noetic-robot-pose-ekf
-# install ros-galactic-robot-pose-ekf # not available for galactic
+install ros-humble-rviz-imu-plugin ros-humble-imu-filter-madgwick
+
 echo "Install robot localization package for use with IMU and wheel odometry"
-install ros-noetic-robot-localization
-install ros-galactic-robot-localization
+install ros-humble-robot-localization
+
 echo "Install ros_numpy package for msgs conversions"
-install ros-noetic-ros-numpy
-# install ros-galactic-ros-numpy # not available for ROS2 yet (see https://github.com/eric-wieser/ros_numpy/issues/20)
-echo "Install ROS control packages (primarily for simulations with Gazebo)"
-install ros-noetic-ros-control ros-noetic-ros-controllers
-echo "Install ROS2 control packages for MoveIt2"
-install ros-galactic-ros2-control ros-galactic-ros2-controllers
-echo "Install ROS teleop packages"
-install ros-noetic-teleop-twist-keyboard
-install ros-galactic-teleop-twist-keyboard
-echo "Install ROS navigation and mapping packages"
-#install ros-noetic-move-base ros-noetic-map-server ros-noetic-amcl ros-noetic-cartographer ros-noetic-cartographer-ros ros-noetic-cartographer-rviz
-install ros-noetic-move-base ros-noetic-move-base-msgs
-install ros-noetic-gmapping ros-noetic-navigation
-# install ros-galactic-move-base ros-galactic-move-base-msgs # not available
-# install ros-galactic-gmapping ros-galactic-navigation # not available
+# install ros-humble-ros-numpy # not available for ROS2 yet (see https://github.com/eric-wieser/ros_numpy/issues/20)
+
+echo "Install control packages for MoveIt 2"
+install ros-humble-ros2-control ros-humble-ros2-controllers
+
+echo "Install teleop packages"
+install ros-humble-teleop-twist-keyboard
+
 echo "Install RPLidar A1M8 packages"
-install ros-noetic-rplidar-ros ros-noetic-rplidar-ros-dbgsym
-install ros-galactic-rplidar-ros ros-galactic-rplidar-ros-dbgsym
+install ros-humble-rplidar-ros ros-humble-rplidar-ros-dbgsym
+
 echo "Install Respeaker and speech recognition packages"
 install python3-pyaudio
-# install ros-galactic-respeaker-ros ros-galactic-ros-speech-recognition # not available
+# install ros-humble-respeaker-ros ros-humble-ros-speech-recognition # not available
+
 echo "Install scan tools for Canonical Scan Matching using the laser_scan_matcher"
-install ros-noetic-scan-tools
-# install ros-galactic-scan-tools # not available
+# install ros-humble-scan-tools # not available
 echo ""
 
-echo "###########################################"
-echo "INSTALLATION OF INTEL D435i"
-echo "###########################################"
-echo "Install dynamic reconfiguration"
-install ros-noetic-ddynamic-reconfigure ros-noetic-ddynamic-reconfigure-python
-echo "Register the librealsense APT server's public key"
-function register_librealsense_apt_server {
-    sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-}
-register_librealsense_apt_server &>> $REDIRECT_LOGFILE
-echo "Add the librealsense APT server to the list of APT repositories"
-sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u &>> $REDIRECT_LOGFILE
-echo "Remove old records in case of upgrading"
-sudo rm -f /etc/apt/sources.list.d/realsense-public.list
-echo "Apt update"
-sudo apt-get --yes update >> $REDIRECT_LOGFILE
-echo "Install librealsense2 packages"
-install librealsense2 librealsense2-dkms librealsense2-udev-rules librealsense2-utils librealsense2-dev librealsense2-dbg
+# echo "###########################################"
+# echo "INSTALLATION OF INTEL D435i"
+# echo "###########################################"
+# echo "Install dynamic reconfiguration"
+# install ros-noetic-ddynamic-reconfigure ros-noetic-ddynamic-reconfigure-python
+# echo "Register the librealsense APT server's public key"
+# function register_librealsense_apt_server {
+#     sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+# }
+# register_librealsense_apt_server &>> $REDIRECT_LOGFILE
+# echo "Add the librealsense APT server to the list of APT repositories"
+# sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u &>> $REDIRECT_LOGFILE
+# echo "Remove old records in case of upgrading"
+# sudo rm -f /etc/apt/sources.list.d/realsense-public.list
+# echo "Apt update"
+# sudo apt-get --yes update >> $REDIRECT_LOGFILE
+# echo "Install librealsense2 packages"
+# install librealsense2 librealsense2-dkms librealsense2-udev-rules librealsense2-utils librealsense2-dev librealsense2-dbg
