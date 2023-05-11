3b752e464eb3625f2d25e2663bf7718f954744d6
hello-chintan <cdesai@hello-robot.com>
2023-05-11 15:13:03 -0700

    Update replace ament_ws script

--- factory/22.04/stretch_create_ament_workspace.sh
+++ factory/22.04/stretch_create_ament_workspace.sh
@@ -18,15 +18,20 @@ done
 REDIRECT_LOGFILE="$REDIRECT_LOGDIR/stretch_create_ament_workspace.`date '+%Y%m%d%H%M'`_redirected.txt"
 
 echo "###########################################"
-echo "CREATING GALACTIC AMENT WORKSPACE at $AMENT_WSDIR"
+echo "CREATING ROS 2 AMENT WORKSPACE at $AMENT_WSDIR"
 echo "###########################################"
 
 echo "Ensuring correct version of ROS is sourced..."
-if [[ $ROS_DISTRO && ! $ROS_DISTRO = "galactic" ]]; then
+if [[ ! $ROS_VERSION -eq 2 ]]; then
     echo "Cannot create workspace while a conflicting ROS version is sourced. Exiting."
     exit 1
 fi
-source /opt/ros/galactic/setup.bash
+
+if [ $ROS_DISTRO = "humble" ]; then
+    source /opt/ros/humble/setup.bash
+else
+    echo "Please source a valid ROS 2 distro. Only ROS 2 Humble supported currently."
+fi
 
 echo "You are about to delete and replace the existing ament workspace. If you have any personal data in the workspace, please create a back up before proceeding."
 prompt_yes_no(){
@@ -50,18 +55,20 @@ echo "Creating the workspace directory..."
 mkdir -p $AMENT_WSDIR/src
 echo "Cloning the workspace's packages..."
 cd $AMENT_WSDIR/src
-vcs import --input ~/stretch_install/factory/20.04/stretch_ros2_galactic.repos >> $REDIRECT_LOGFILE
+vcs import --input ~/stretch_install/factory/22.04/stretch_ros2_humble.repos >> $REDIRECT_LOGFILE
 echo "Fetch ROS packages' dependencies (this might take a while)..."
 cd $AMENT_WSDIR/
-rosdep install --rosdistro=galactic -iy --skip-keys="librealsense2" --from-paths src &>> $REDIRECT_LOGFILE
+rosdep install --rosdistro=humble -iyr --skip-keys="librealsense2" --from-paths src &>> $REDIRECT_LOGFILE
+
 # TODO: replace these hacks
-echo "Fetch calibration data from catkin_ws..."
-if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf" ]; then
-    cp ~/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf $AMENT_WSDIR/src/stretch_ros2/stretch_description/urdf/
-fi
-if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml" ]; then
-    cp ~/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml $AMENT_WSDIR/src/stretch_ros2/stretch_core/config/
-fi
+# echo "Fetch calibration data from catkin_ws..."
+# if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf" ]; then
+#     cp ~/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf $AMENT_WSDIR/src/stretch_ros2/stretch_description/urdf/
+# fi
+# if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml" ]; then
+#     cp ~/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml $AMENT_WSDIR/src/stretch_ros2/stretch_core/config/
+# fi
+
 echo "Compile the workspace (this might take a while)..."
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release &>> $REDIRECT_LOGFILE
 echo "Source setup.bash file..."
@@ -69,6 +76,7 @@ source $AMENT_WSDIR/install/setup.bash
 echo "Update ~/.bashrc dotfile to source workspace..."
 echo "#source $AMENT_WSDIR/install/setup.bash" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+
 # TODO:
 #echo "Updating meshes in stretch_ros to this robot's batch..."
 #. /etc/hello-robot/hello-robot.conf
