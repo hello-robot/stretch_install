ea5f504f1f3895063f0ab61114e824a2fb629f06
Chintan Desai <cdesai@hello-robot.com>
2023-06-27 16:22:20 -0700

    Add moveit py apt sources for humble

--- factory/22.04/stretch_install_system.sh
+++ factory/22.04/stretch_install_system.sh
@@ -74,6 +74,10 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 echo "Setting up sources.list"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+echo "Setting up moveit py for humble sources"
+echo "deb [trusted=yes] https://raw.githubusercontent.com/moveit/moveit2_packages/jammy-humble/ ./" | sudo tee /etc/apt/sources.list.d/moveit_moveit2_packages.list
+echo "yaml https://raw.githubusercontent.com/moveit/moveit2_packages/jammy-humble/local.yaml humble" | sudo tee /etc/ros/rosdep/sources.list.d/1-moveit_moveit2_packages.list
+
 echo "Apt update"
 sudo apt-get --yes update >> $REDIRECT_LOGFILE
 
