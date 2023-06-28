5b3c88de830784b6ffbed6fe6f581dc560c9cdbf
Chintan Desai <cdesai@hello-robot.com>
2023-06-28 16:59:51 -0700

    Set fleet path and ID and fetch and update calibration files

--- factory/22.04/stretch_create_ament_workspace.sh
+++ factory/22.04/stretch_create_ament_workspace.sh
@@ -57,15 +57,6 @@ echo "Fetch ROS packages' dependencies (this might take a while)..."
 cd $AMENT_WSDIR/
 rosdep install --rosdistro=humble -iyr --skip-keys="librealsense2" --from-paths src &>> $REDIRECT_LOGFILE
 
-# TODO: replace these hacks
-# echo "Fetch calibration data from catkin_ws..."
-# if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf" ]; then
-#     cp ~/catkin_ws/src/stretch_ros/stretch_description/urdf/stretch.urdf $AMENT_WSDIR/src/stretch_ros2/stretch_description/urdf/
-# fi
-# if [ -f "$HOME/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml" ]; then
-#     cp ~/catkin_ws/src/stretch_ros/stretch_core/config/controller_calibration_head.yaml $AMENT_WSDIR/src/stretch_ros2/stretch_core/config/
-# fi
-
 echo "Compile the workspace (this might take a while)..."
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release &>> $REDIRECT_LOGFILE
 echo "Source setup.bash file..."
@@ -74,17 +65,22 @@ echo "Update ~/.bashrc dotfile to source workspace..."
 echo "source $AMENT_WSDIR/install/setup.bash" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 
-# TODO:
-#echo "Updating meshes in stretch_ros to this robot's batch..."
-#. /etc/hello-robot/hello-robot.conf
-#export HELLO_FLEET_ID HELLO_FLEET_ID
-#export HELLO_FLEET_PATH=${HOME}/stretch_user
-#$CATKIN_WSDIR/src/stretch_ros/stretch_description/meshes/update_meshes.py >> $REDIRECT_LOGFILE
-#echo "Setup uncalibrated robot URDF..."
-#bash -i $CATKIN_WSDIR/src/stretch_ros/stretch_calibration/nodes/update_uncalibrated_urdf.sh >> $REDIRECT_LOGFILE
-#echo "Setup calibrated robot URDF..."
-#bash -i $CATKIN_WSDIR/src/stretch_ros/stretch_calibration/nodes/update_with_most_recent_calibration.sh >> $REDIRECT_LOGFILE
+echo "Updating meshes in stretch_ros to this robot's batch..."
+. /etc/hello-robot/hello-robot.conf
+export HELLO_FLEET_ID HELLO_FLEET_ID
+export HELLO_FLEET_PATH=${HOME}/stretch_user
+#TODO:
+#$AMENT_WSDIR/src/stretch_ros2/stretch_description/meshes/update_meshes.py >> $REDIRECT_LOGFILE
+
+echo "Setup uncalibrated robot URDF..."
+ros2 run stretch_calibration update_uncalibrated_urdf >> $REDIRECT_LOGFILE
+
+echo "Setup calibrated robot URDF..."
+ros2 run stretch_calibration update_with_most_recent_calibration >> $REDIRECT_LOGFILE
+
+colcon build >> $REDIRECT_LOGFILE
+
+#TODO:
 #echo "Compiling FUNMAP's Cython code..."
 #cd $CATKIN_WSDIR/src/stretch_ros/stretch_funmap/src/stretch_funmap
 #./compile_cython_code.sh &>> $REDIRECT_LOGFILE
-
