18efec8a318cdbb8154c3ae6dfd82d76f8623698
hello-chintan <cdesai@hello-robot.com>
2023-08-15 17:11:39 -0700

    Install arduino-cli for firmware flash

--- factory/22.04/stretch_install_firmware.sh
+++ factory/22.04/stretch_install_firmware.sh
@@ -10,6 +10,14 @@ REDIRECT_LOGFILE="$REDIRECT_LOGDIR/stretch_install_firmware.`date '+%Y%m%d%H%M'`
 echo "###########################################"
 echo "INSTALLATION OF STRETCH FIRMWARE"
 echo "###########################################"
+
+#To install Arduino CLI
+echo "Installing arduino-cli"
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$HOME/.local/bin/ sh
+$HOME/.local/bin/arduino-cli config init
+$HOME/.local/bin/arduino-cli core install arduino:samd@1.6.21
+sed -i -e 's#Arduino#repos/stretch_firmware/arduino#g' ~/.arduino15/arduino-cli.yaml
+
 source ~/.bashrc
 echo "Read ttyACMx mapping"
 REx_firmware_flash.py --map &>> $REDIRECT_LOGFILE
