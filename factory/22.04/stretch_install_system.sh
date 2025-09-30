0ea6c73471d6d731702e63c1fe11df010f75d062
Shehab <sattia@hello-robot.com>
2025-09-30 13:34:24 -0400

    Added -f so that sudo rm does not error if files don't exist.

--- factory/22.04/stretch_install_system.sh
+++ factory/22.04/stretch_install_system.sh
@@ -74,8 +74,8 @@ echo "###########################################"
 echo "INSTALLATION OF ROS 2 HUMBLE"
 echo "###########################################"
 echo "Setting up keys"
-sudo rm /usr/share/keyrings/ros-archive-keyring.gpg
-sudo rm /etc/apt/sources.list.d/ros2.list
+sudo rm -f /usr/share/keyrings/ros-archive-keyring.gpg
+sudo rm -f /etc/apt/sources.list.d/ros2.list
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb" &>> $REDIRECT_LOGFILE
 sudo dpkg -i /tmp/ros2-apt-source.deb >> $REDIRECT_LOGFILE
